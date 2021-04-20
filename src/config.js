e591be8a2ad17e918c3ba183cc537a5642265fb2
Zack Jameson <zacharymjameson@gmail.com>
2021-04-20 08:34:04 -0400

    vercel production preparation

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+  API_ENDPOINT: `https://frozen-savannah-76224.herokuapp.com/`,
   API_KEY: process.env.REACT_APP_API_KEY,
 };
